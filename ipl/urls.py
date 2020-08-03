48301469a477f0a99740c948dde1edb2d38dc711
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-08-03 19:20:16 +0530

    added bg

--- ipl/urls.py
+++ ipl/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ipl')
+    path('', views.home, name='home'),
+    path('prediction', views.prediction, name='prediction')
 ]
--- ipl/views.py
+++ ipl/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def prediction(request):
+    return render(request, 'prediction.html')
