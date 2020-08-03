b75cd44ebe48275bb6a0b880993c423cd9ada8b0
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-08-03 23:18:36 +0530

    added api part

--- api/views.py
+++ api/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+
+@csrf_exempt
+@require_http_methods(["POST"])
 def predict_score(request):
-    return JsonResponse({'status': 'success', 'data':{'predicted-socre': 200}})
+    # if all(name in grades for name in students):
+    keys = ['runs', 'wickets', 'runs_last_5', 'wickets_last_5', 'striker', 'non-striker', 'bat_team', 'bowl_team']
+    if all(key in request.POST for key in keys):
+        return JsonResponse({'status': 'success', 'data':{'predicted-socre': 200}})
+    else:
+        return JsonResponse({'status': 'failed'})
