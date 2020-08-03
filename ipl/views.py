86f6e7e462ecffdc5742e3aa036a43285a9ac280
Hrishikesh <hrishipotter123@gmail.com>
2020-08-03 22:30:44 +0530

    added other inputs

--- ipl/views.py
+++ ipl/views.py
@@ -8,6 +8,9 @@ def home(request):
 def prediction(request):
     if request.method == 'POST':
         runs = request.POST['runs']
+        wickets = request.POST['wickets']
+        runs_last_5 = request.POST['runs_last_5']
+        wickets_last_5 = request.POST['wickets_last_5']
         return render(request, 'prediction.html', {'new_runs': runs})
     else:
         return render(request, 'prediction.html')
