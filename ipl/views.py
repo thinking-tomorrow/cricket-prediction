8292fbc0df6f7494374db309fc79c3699351d2cc
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-08-04 11:43:05 +0530

    added overs input

--- ipl/views.py
+++ ipl/views.py
@@ -9,13 +9,15 @@ def prediction(request):
     if request.method == 'POST':
         runs = request.POST['runs']
         wickets = request.POST['wickets']
+        overs = request.POST['overs']
         runs_last_5 = request.POST['runs_last_5']
         wickets_last_5 = request.POST['wickets_last_5']
         striker = request.POST['striker']
         non_striker = request.POST['non-striker']
         bat_team = request.POST['bat_team']
         bowl_team = request.POST['bowl_team']
-        return render(request, 'prediction.html', {'runs': runs, 'wickets':wickets, 'runs_last_5':runs_last_5,
+
+        return render(request, 'prediction.html', {'runs': runs, 'wickets':wickets, 'overs': overs, 'runs_last_5':runs_last_5,
                                                    'wickets_last_5':wickets_last_5,'striker':striker,'non_striker':non_striker,
                                                    'bat_team':bat_team,'bowl_team':bowl_team})
     else:
