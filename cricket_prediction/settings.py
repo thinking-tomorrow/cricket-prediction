f4fd67504fc48e17d5750ccbf5de5ed9d983ae35
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-08-08 18:34:27 +0530

    added mysql database

--- cricket_prediction/settings.py
+++ cricket_prediction/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cricket_prediction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cricket_prediction',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
