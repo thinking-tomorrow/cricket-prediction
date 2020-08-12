711dd07ba25928734f4cd0cb59f7caa25628def0
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-12 11:08:14 +0530

    added max winners

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('predict-score', views.predict_score, name='home'),
     path('winners',views.matches,name='winners'),
+    path('maximum_winners',views.maximum_winners,name='maximum_winners')
 ]
