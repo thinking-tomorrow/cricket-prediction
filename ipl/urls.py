0167f97b5c5db396963bdc5620ae51a01569a50d
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-22 22:28:53 +0530

    added func in url

--- ipl/urls.py
+++ ipl/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('prediction', views.prediction, name='prediction'),
     path('info',views.info,name='info'),
-    path('visualization', views.visualization, name='visualization')
+    path('visualization', views.visualization, name='visualization'),
+    path('call_func',views.call_func,name='call_func')
     # path('result', views.result, name='result')
 ]
