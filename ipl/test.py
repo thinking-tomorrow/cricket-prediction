e1a3dff729244099fcb4d55097cc5ea2197f8569
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-22 14:41:46 +0530

    added test file

--- ipl/test.py
+++ ipl/test.py
@@ -4,6 +4,17 @@ import requests
 source = requests.get("https://www.icccricketschedule.com/vivo-ipl-2020-schedule-team-venue-time-table-pdf-point-table-ranking-winning-prediction/").text
 soup = BeautifulSoup(source,'lxml')
 
-table = soup.find('tr')
+table = soup.find('table')
 
-print(table)
+'''for matches in table:
+
+    rows = matches.find_all('td')
+    print(rows[1])'''
+
+matches_rows = table.find_all('tr')
+
+for matches in matches_rows:
+
+    new = matches.find_all('td')
+    new = list(map(lambda x:str(x.text).replace(',', ''), new))
+    print(new[1])
