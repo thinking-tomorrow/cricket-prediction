f622eb7aa8efcd512cce39354c0fe66837fd4f8a
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-22 18:09:25 +0530

    started adding in views

--- ipl/views.py
+++ ipl/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from bs4 import BeautifulSoup
 import requests
 
+
+def scrape_schedule():
+    from bs4 import BeautifulSoup
+import requests
+
+source = requests.get("https://www.icccricketschedule.com/vivo-ipl-2020-schedule-team-venue-time-table-pdf-point-table-ranking-winning-prediction/").text
+soup = BeautifulSoup(source,'lxml')
+
+table = soup.find('table')
+
+'''for matches in table:
+
+    rows = matches.find_all('td')
+    print(rows[1])'''
+
+matches_rows = table.find_all('tr')
+matches_rows=matches_rows[1:]
+
+for matches in matches_rows:
+
+    new = matches.find_all('td')
+    new = list(map(lambda x:str(x.text).replace(',', ''), new))
+    print(new[0])
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
