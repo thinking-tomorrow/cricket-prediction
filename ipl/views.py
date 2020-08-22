c4aaf74154b6818bda6f3b6219be44acaf86d404
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-22 18:14:55 +0530

    added func

--- ipl/views.py
+++ ipl/views.py
@@ -2,30 +2,37 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 import requests
-
+from models import Schedule
 
 def scrape_schedule():
-    from bs4 import BeautifulSoup
-import requests
 
-source = requests.get("https://www.icccricketschedule.com/vivo-ipl-2020-schedule-team-venue-time-table-pdf-point-table-ranking-winning-prediction/").text
-soup = BeautifulSoup(source,'lxml')
+    source = requests.get("https://www.icccricketschedule.com/vivo-ipl-2020-schedule-team-venue-time-table-pdf-point-table-ranking-winning-prediction/").text
+    soup = BeautifulSoup(source,'lxml')
 
-table = soup.find('table')
+    table = soup.find('table')
 
-'''for matches in table:
+    '''for matches in table:
 
-    rows = matches.find_all('td')
-    print(rows[1])'''
+        rows = matches.find_all('td')
+        print(rows[1])'''
 
-matches_rows = table.find_all('tr')
-matches_rows=matches_rows[1:]
+    matches_rows = table.find_all('tr')
+    matches_rows=matches_rows[1:]
 
-for matches in matches_rows:
+    for matches in matches_rows:
 
-    new = matches.find_all('td')
-    new = list(map(lambda x:str(x.text).replace(',', ''), new))
-    print(new[0])
+        new = matches.find_all('td')
+        new = list(map(lambda x:str(x.text).replace(',', ''), new))
+        print(new[0])
+
+        matches = Schedule()
+        matches.team = new[1]
+        matches.date = new[2]
+        matches.day = new[3]
+        matches.time = new[4]
+        matches.city = new[5]
+
+        matches.save()        
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
