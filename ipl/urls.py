efc25930cd9083d1713a047b862d2a0eb7a2460b
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-25 22:30:52 +0530

    added urls

--- ipl/urls.py
+++ ipl/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('prediction', views.prediction, name='prediction'),
     path('info',views.info,name='info'),
     path('visualization', views.visualization, name='visualization'),
-    path('call_func',views.call_func,name='call_func')
+    path('call_func',views.call_func,name='call_func'),
+    path('schedule',views.schedule,name='schedule')
     # path('result', views.result, name='result')
 ]
