93f55fbb8a81bd1bdc7905259ee9d59562a0cc04
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-08-26 08:57:17 +0530

    change

--- ipl/urls.py
+++ ipl/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('prediction', views.prediction, name='prediction'),
     path('info',views.info,name='info'),
     path('visualization', views.visualization, name='visualization'),
-    path('call_func',views.call_func,name='call_func')
+    path('call_func',views.call_func,name='call_func'),
     # path('result', views.result, name='result'),
     path('schedule',views.schedule,name='schedule')
 ]
